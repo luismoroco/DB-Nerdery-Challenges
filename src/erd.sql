af9c6ff44b747bcddcd1deb0b07aa92b7a0b38c3
lmoroco <lmorocoramos@gmail.com>
2024-12-18 17:23:46 -0500

    refactor: use enum instead tables. Remove file management.

--- src/erd.sql
+++ src/erd.sql
@@ -8,121 +8,36 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- User
-CREATE TABLE "user_type" (
-  user_type_id BIGINT PRIMARY KEY, name TEXT,
-  label TEXT
-);
-INSERT INTO user_type
-VALUES
-  (1, 'MANAGER', 'Manager'),
-  (2, 'CLIENT', 'Client');
-
+CREATE TYPE user_type AS ENUM ('MANAGER', 'CLIENT');
 CREATE TABLE "user" (
   user_id BIGSERIAL PRIMARY KEY NOT NULL,
-  user_type_id BIGINT NOT NULL,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
+  user_type USER_TYPE NOT NULL,
+  first_name VARCHAR(100) NOT NULL CHECK (char_length(first_name) > 1),
+  last_name VARCHAR(100) NOT NULL CHECK (char_length(last_name) > 1),
+  email VARCHAR(254) UNIQUE NOT NULL CHECK (position('@' IN email) > 1),
   password TEXT NOT NULL,
   created_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  FOREIGN KEY (user_type_id) REFERENCES "user_type"(user_type_id) ON DELETE CASCADE
+  updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 CREATE TRIGGER update_user_updated_at BEFORE
 UPDATE
   ON "user" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
 -- Credentials recovery
-CREATE TABLE "user_credential_type" (
-  user_credential_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO user_credential_type
-VALUES
-  (1, 'PASSWORD', 'Password');
-
+CREATE TYPE user_credential_type AS ENUM ('PASSWORD');
 CREATE TABLE "user_credential_recovery" (
   user_id BIGINT NOT NULL,
-  user_credential_type_id BIGINT NOT NULL,
-  token TEXT NOT NULL,
+  user_credential_type USER_CREDENTIAL_TYPE NOT NULL,
+  token TEXT NOT NULL CHECK (char_length(token) > 1),
   used_at TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL,
   FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (user_credential_type_id) REFERENCES "user_credential_type"(user_credential_type_id) ON DELETE CASCADE,
-  PRIMARY KEY (user_id, user_credential_type_id)
+  PRIMARY KEY (user_id, user_credential_type)
 );
 
--- Notification
-CREATE TABLE "notification_event_type" (
-  notification_event_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT, spec JSONB NOT NULL
-);
-INSERT INTO notification_event_type
-VALUES
-  (1, 'PASSWORD_RECOVERY', 'Password recovery', '{}'),
-  (2, 'LOW_STOCK', 'Low stock', '{"minimumStock": 3}');
-
-CREATE TABLE "notification_state_type" (
-  notification_state_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO notification_state_type
-VALUES
-  (1, 'PENDING', 'Pending'),
-  (2, 'SENT', 'Sent'),
-  (3, 'FAILED', 'Failed');
-
-CREATE TABLE "notification_provider" (
-  notification_provider_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
-  label TEXT NOT NULL
-);
-INSERT INTO notification_provider
-VALUES
-  (1, 'G-MAIL', 'G-Mail');
-
-CREATE TABLE "notification" (
-  notification_id BIGSERIAL PRIMARY KEY NOT NULL,
-  notification_provider_id BIGINT NOT NULL,
-  notification_event_type_id BIGINT NOT NULL,
-  notification_state_type_id BIGINT NOT NULL,
-  user_id BIGINT NOT NULL,
-  body JSONB NOT NULL,
-  FOREIGN KEY (notification_provider_id) REFERENCES "notification_provider"(notification_provider_id) ON DELETE CASCADE,
-  FOREIGN KEY (notification_event_type_id) REFERENCES "notification_event_type"(notification_event_type_id) ON DELETE CASCADE,
-  FOREIGN KEY (notification_state_type_id) REFERENCES "notification_state_type"(notification_state_type_id) ON DELETE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES "user"(user_id)
-);
-
--- File
-CREATE TABLE "file_type" (
-  file_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO file_type
-VALUES
-  (1, 'IMAGE', 'Image');
-
-CREATE TABLE "storage_provider" (
-  storage_provider_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO storage_provider
-VALUES
-  (1, 'S3', 'Amazon S3');
-
-CREATE TABLE "file" (
-  file_id BIGINT PRIMARY KEY NOT NULL,
-  storage_provider_id BIGINT NOT NULL,
-  file_type_id BIGINT NOT NULL,
-  url TEXT NOT NULL,
-  FOREIGN KEY (storage_provider_id) REFERENCES "storage_provider"(storage_provider_id),
-  FOREIGN KEY (file_type_id) REFERENCES "file_type"(file_type_id)
-);
-
--- Currency
+-- Currency [short_name = ISO 4217]
 CREATE TABLE "currency" (
   currency_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
+  short_name VARCHAR(3), name TEXT
 );
 INSERT INTO currency
 VALUES
@@ -132,9 +47,9 @@ VALUES
 -- Products
 CREATE TABLE "product" (
   product_id BIGSERIAL PRIMARY KEY NOT NULL,
-  name TEXT UNIQUE NOT NULL,
-  description TEXT NOT NULL,
-  price NUMERIC(12, 2) NOT NULL,
+  name TEXT UNIQUE NOT NULL CHECK (char_length(name) > 1),
+  description TEXT NOT NULL CHECK (char_length(description) > 1),
+  price NUMERIC(12, 2) NOT NULL CHECK (price >= 0),
   enabled BOOLEAN DEFAULT TRUE,
   archived BOOLEAN DEFAULT FALSE,
   featured BOOLEAN DEFAULT FALSE,
@@ -147,20 +62,19 @@ CREATE TRIGGER update_product_updated_at BEFORE
 UPDATE
   ON "product" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
-CREATE TABLE "product_resource" (
-  product_resource_id BIGSERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE "product_image" (
+  product_image_id BIGSERIAL PRIMARY KEY NOT NULL,
   product_id BIGINT NOT NULL,
-  file_id BIGINT NOT NULL,
+  url TEXT NOT NULL CHECK (char_length(url) > 1),
   number INT NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES "product"(product_id),
-  FOREIGN KEY (file_id) REFERENCES "file"(file_id)
+  FOREIGN KEY (product_id) REFERENCES "product"(product_id) ON DELETE CASCADE
 );
 
 -- Category
 CREATE TABLE "category" (
   category_id BIGSERIAL PRIMARY KEY NOT NULL,
-  name TEXT UNIQUE NOT NULL,
-  featured BOOLEAN,
+  name TEXT UNIQUE NOT NULL CHECK (char_length(name) > 1),
+  featured BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -168,13 +82,12 @@ CREATE TRIGGER update_category_updated_at BEFORE
 UPDATE
   ON "category" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
-CREATE TABLE "category_resource" (
-  category_resource_id BIGSERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE "category_image" (
+  category_image_id BIGSERIAL PRIMARY KEY NOT NULL,
   category_id BIGINT NOT NULL,
-  file_id BIGINT NOT NULL,
+  url TEXT NOT NULL CHECK (char_length(url) > 1),
   number INT NOT NULL,
-  FOREIGN KEY (category_id) REFERENCES "category"(category_id),
-  FOREIGN KEY (file_id) REFERENCES "file"(file_id)
+  FOREIGN KEY (category_id) REFERENCES "category"(category_id) ON DELETE CASCADE
 );
 
 CREATE TABLE "category_product" (
@@ -186,26 +99,16 @@ CREATE TABLE "category_product" (
 );
 
 -- Order
-CREATE TABLE "order_state_type" (
-  order_state_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO order_state_type
-VALUES
-  (1, 'PENDING', 'Pending'),
-  (2, 'PAID', 'Paid'),
-  (3, 'FAILED', 'Failed');
-
+CREATE TYPE order_status_type AS ENUM ('PENDING', 'PAID', 'FAILED');
 CREATE TABLE "order" (
   order_id BIGSERIAL PRIMARY KEY NOT NULL,
   user_id BIGINT NOT NULL,
-  order_state_type_id BIGINT NOT NULL,
+  order_status_type ORDER_STATUS_TYPE NOT NULL,
   currency_id BIGINT NOT NULL,
-  amount NUMERIC(12, 2) NOT NULL,
+  amount NUMERIC(12, 2) NOT NULL CHECK (amount >= 0),
   created_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (order_state_type_id) REFERENCES "order_state_type"(order_state_type_id) ON DELETE CASCADE,
   FOREIGN KEY (currency_id) REFERENCES "currency"(currency_id) ON DELETE CASCADE
 );
 CREATE TRIGGER update_order_updated_at BEFORE
@@ -216,80 +119,42 @@ CREATE TABLE "order_item" (
   order_item_id BIGSERIAL PRIMARY KEY NOT NULL,
   order_id BIGINT NOT NULL,
   product_id BIGINT NOT NULL,
-  name TEXT NOT NULL,
-  unit_price NUMERIC(12, 2) NOT NULL,
+  name TEXT NOT NULL CHECK (char_length(name) > 1),
+  unit_price NUMERIC(12, 2) NOT NULL CHECK (unit_price >= 0),
   quantity INT CHECK (quantity >= 1) NOT NULL,
   FOREIGN KEY (order_id) REFERENCES "order"(order_id) ON DELETE CASCADE,
   FOREIGN KEY (product_id) REFERENCES "product"(product_id) ON DELETE CASCADE
 );
 
 -- Payment
-CREATE TABLE "payment_method_type" (
-  payment_method_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO payment_method_type
-VALUES
-  (1, 'CARD', 'Card');
-
+CREATE TYPE payment_method_type AS ENUM ('CARD');
 CREATE TABLE "payment_provider" (
   payment_provider_id BIGINT PRIMARY KEY NOT NULL,
-  payment_method_type_id BIGINT NOT NULL,
+  payment_method_type PAYMENT_METHOD_TYPE NOT NULL,
   name TEXT NOT NULL,
-  label TEXT NOT NULL,
-  spec JSONB NOT NULL,
-  enabled BOOLEAN NOT NULL,
-  FOREIGN KEY (payment_method_type_id) REFERENCES "payment_method_type"(payment_method_type_id) ON DELETE CASCADE
+  enabled BOOLEAN NOT NULL
 );
 INSERT INTO payment_provider
 VALUES
-  (1, 1, 'STRIPE CHECKOUT', 'Stripe Checkout', '{"supportedCurrencies": ["USD", "EUR"]}', true);
+  (1, 'CARD', 'STRIPE', true);
 
-CREATE TABLE "payment_state_type" (
-  payment_state_type_id BIGINT PRIMARY KEY NOT NULL,
-  name TEXT, label TEXT
-);
-INSERT INTO payment_state_type
-VALUES
-    (1, 'PENDING', 'Pending'),
-    (2, 'PAID', 'Paid'),
-    (3, 'FAILED', 'Failed'),
-    (4, 'CANCELED', 'Canceled');
-
+CREATE TYPE payment_status_type AS ENUM ('PENDING', 'PAID', 'FAILED', 'CANCELED');
 CREATE TABLE "payment" (
   payment_id BIGSERIAL PRIMARY KEY NOT NULL,
   order_id BIGINT NOT NULL,
   payment_provider_id BIGINT NOT NULL,
-  payment_state_type_id BIGINT NOT NULL,
+  payment_status_type PAYMENT_STATUS_TYPE NOT NULL,
   initialization_data JSONB,
   transaction_data JSONB,
   created_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   paid_at TIMESTAMP(3) WITH TIME ZONE,
   FOREIGN KEY (order_id) REFERENCES "order"(order_id),
-  FOREIGN KEY (payment_provider_id) REFERENCES "payment_provider"(payment_provider_id),
-  FOREIGN KEY (payment_state_type_id) REFERENCES "payment_state_type"(payment_state_type_id)
+  FOREIGN KEY (payment_provider_id) REFERENCES "payment_provider"(payment_provider_id)
 );
 CREATE TRIGGER update_payment_updated_at BEFORE
 UPDATE
   ON "payment" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-
--- Store
-CREATE TABLE "store_configuration" (
-  store_id BIGINT PRIMARY KEY NOT NULL,
-  currency_id BIGINT NOT NULL,
-  notification_provider_id BIGINT NOT NULL,
-  name TEXT NOT NULL,
-  updated_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  FOREIGN KEY (currency_id) REFERENCES "currency"(currency_id),
-  FOREIGN KEY (notification_provider_id) REFERENCES "notification_provider"(notification_provider_id)
-);
-CREATE TRIGGER update_store_configuration_updated_at BEFORE
-UPDATE
-  ON "store_configuration" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-INSERT INTO store_configuration
-VALUES
-  (1, 1, 1, 'Shop');
 
 -- Shopping Cart
 CREATE TABLE "shopping_cart" (
@@ -301,8 +166,8 @@ CREATE TABLE "shopping_cart_item" (
   shopping_cart_item_id BIGSERIAL PRIMARY KEY NOT NULL,
   shopping_cart_id BIGINT NOT NULL,
   product_id BIGINT NOT NULL,
-  quantity INT NOT NULL,
-  number INT NOT NULL,
+  quantity INT NOT NULL CHECK (quantity >= 1),
+  number INT NOT NULL CHECK (number >= 0),
   FOREIGN KEY (shopping_cart_id) REFERENCES "shopping_cart"(shopping_cart_id) ON DELETE CASCADE,
   FOREIGN KEY (product_id) REFERENCES "product"(product_id) ON DELETE CASCADE
 );
@@ -313,5 +178,5 @@ CREATE TABLE "product_like" (
   user_id BIGINT NOT NULL,
   FOREIGN KEY (product_id) REFERENCES "product"(product_id) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
-  PRIMARY KEY (product_id, user_id)
+  UNIQUE (product_id, user_id)
 );
